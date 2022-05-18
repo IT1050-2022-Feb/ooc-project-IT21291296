7bc7298ba308bf635ec6713c7ff59f9d2065d7b7
IT21291296 <99247893+IT21291296@users.noreply.github.com>
2022-05-19 00:31:16 +0530

    Add files via upload

---
+++ account.cpp
@@ -0,0 +1,16 @@
+//Account class implementation
+#include "account.h"
+Account::Account() 
+   {
+    strcpy(name,"");
+    strcpy(address,"");
+    strcpy(dob, "");
+    contactNumber = 0;
+    strcpy(email,"");
+   };
+
+   void Account::addAccountDetails(){
+   };
+
+   void Account::UpdateAccount(){
+   };
---
+++ manager.cpp
@@ -0,0 +1,29 @@
+#include "manager.h"
+Manager::Manager(){
+	strcpy(name," ");
+    strcpy(address," ");
+    strcpy(dob, " ");
+    conatcNumber = 0;
+    strcpy(email,"");
+    strcpy(ManagerPassword,"");
+    strcpy(ManagerID,"");
+    strcpy(ReportID,"");
+}
+
+
+	void Manager::GenerateEmpReports(Database *D1){
+	};
+ 
+    void Manager::GenerateVehiReports(Database *D1){
+	};
+    	
+    void Manager::GeneratePayReports(Database *D1){
+	};
+    	
+    	
+    void Manager::AddEmployees(){
+	};
+    
+    void Manager::RemoveEmployees(){
+	};
+
